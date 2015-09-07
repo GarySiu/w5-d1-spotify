299be71d6ed56520aa8ecf022f7d7ef31756dd9e
Gary Siu <Gary@untypeable.com>
2015-09-07 23:04:03 +0100

    Tried to make the search more responsive without being excessive

--- js/main.js
+++ js/main.js
@@ -6,24 +6,30 @@ $(document).ready(function() {
   var searchBox = $('#search-box');
   var typeSelect = $('#type-select');
   var results = $('#results');
+  var tempSearch = searchBox.val();
 
-getResults = function() {
-  event.preventDefault(); // a moment's silence for Gui's dignity
-  results.empty();
-  if(typeSelect.val() === 'default') {
-    results.append('<div>Please pick a search type</div>');
-    return;
+  cautiousGetResults = function() {
+    if(searchBox.val() !== tempSearch) { getResults() }
   }
 
-  $.get('https://api.spotify.com/v1/search?q=' + searchBox.val() + '&type=' + typeSelect.val(), function(response) {
+  getResults = function() {
+    event.preventDefault(); // a moment's silence for Gui's dignity
+    results.empty();
+    if(typeSelect.val() === 'default') {
+      results.append('<div>Please pick a search type</div>');
+      return;
+    }
+
+    $.get('https://api.spotify.com/v1/search?q=' + searchBox.val() + '&type=' + typeSelect.val(), function(response) {
       $.each(response[ typeSelect.val() + 's'].items, function(index, item) {
           results.append('<div>' + item.name + '</div>');
       })
-  })
-}
+    })
+  }
 
   submitButton.on('click', getResults);
   typeSelect.on('change', getResults);
-  searchBox.on('change', getResults);
+  searchBox.on('keydown', function() { tempSearch = searchBox.val() }); // Let's make sure something is actually typed before spamming AJAX calls.
+  searchBox.on('keyup', cautiousGetResults); // possibly a terrible idea because there will be a LOT of API calls but looks like Google Instant now!
 
 })
