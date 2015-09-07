c917efb1462b255be6801013b624089b732c88c9
Gary Siu <Gary@untypeable.com>
2015-09-07 18:10:37 +0100

    Basic eventhandler test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-
   // console.log('Hello world!')
+
+  // define variables/HTML elements
+  var submitButton = $('#submit-button');
+  var searchBox = $('#search-box');
+  var typeSelect = $('#type-select');
+  var results = $('#results');
+
+getResults = function() {
+  event.preventDefault(); // a moment's silence for Gui's dignity
+  var result = '<h2>Tada!</h2>'
+  results.append(result);
+}
+
+  submitButton.on('click', getResults)
 
 })
