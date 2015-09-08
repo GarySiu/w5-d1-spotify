11d215a73bd0c3e58a692ec0a715ab1ed2e1dd18
Gary Siu <Gary@untypeable.com>
2015-09-08 10:15:45 +0100

    Added the bonus task they actually wanted. Spec was ambiguous

--- js/main.js
+++ js/main.js
@@ -18,12 +18,12 @@ $(document).ready(function() {
 
     $.get('https://api.spotify.com/v1/search?q=' + searchBox.val() + '&type=' + typeSelect.val(), function(response) {
       $.each(response[ typeSelect.val() + 's'].items, function(index, item) {
-        results.append('<div class="search-results">' + item.name + '</div>');
-        
+        results.hide().append('<div class="search-results">' + item.name + '</div>').fadeIn('slow');
+
         if(typeSelect.val() === 'track') {
           // debugger;
-          results.append('<img src="' + item.album.images[1].url + '">');
-          results.append('<div><audio src="' + item.preview_url + '" controls></audio></div>');
+          results.hide().append('<img src="' + item.album.images[1].url + '" height=" ' + item.album.images[1].height + '" width=" ' + item.album.images[1].width + ' ">').fadeIn('slow');
+          results.hide().append('<div><audio src="' + item.preview_url + '" controls></audio></div>').fadeIn('slow');
         }
       })
     })
@@ -37,8 +37,8 @@ $(document).ready(function() {
   typeSelect.on('change', getResults);
   searchBox.on('keydown', function() { tempSearch = searchBox.val() }); // Let's make sure something is actually typed before spamming AJAX calls.
   searchBox.on('keyup', cautiousGetResults); // possibly a terrible idea because there will be a LOT of API calls but looks like Google Instant now!
-  searchBox.on('blur', function() {searchBox.fadeTo('slow', 0.3)}); // I don't like this but hey, free bonus task!
-  searchBox.on('mouseout', function() {searchBox.fadeTo('slow', 0.3)});
-  searchBox.on('focus', function() {searchBox.fadeTo('slow', 1)});
-  searchBox.on('mouseover', function() {searchBox.fadeTo('slow', 1)});
+  // searchBox.on('blur', function() {searchBox.fadeTo('slow', 0.3)}); // I don't like this but hey, free bonus task!
+  // searchBox.on('mouseout', function() {searchBox.fadeTo('slow', 0.3)});
+  // searchBox.on('focus', function() {searchBox.fadeTo('slow', 1)});
+  // searchBox.on('mouseover', function() {searchBox.fadeTo('slow', 1)});
 })
